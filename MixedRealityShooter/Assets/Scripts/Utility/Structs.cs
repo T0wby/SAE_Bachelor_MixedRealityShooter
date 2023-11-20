420711285f0da9b701f93de7ffb660f915f355f7
Towby <tobiasnuehlen@gmx.de>
2023-11-20 16:44:56 +0100

    -Added scripts from the anchor branche

---
+++ MixedRealityShooter/Assets/Scripts/Utility/Structs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utility
+{
+    [Serializable]
+    public struct SPLacedObject
+    {
+        public Guid UniqueId;
+        public bool IsWall;
+        public Vector3 Scaling;
+    }
+}
